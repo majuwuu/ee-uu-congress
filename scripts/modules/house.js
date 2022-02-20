06aa7b1906c4f5ab9511d861c8f4027a9d65dca5
Maria Jose Zelaya <majo@HP250G7NotebookPCa13ac586.fibertel.com.ar>
2022-02-20 20:11:46 -0300

    dinamic tables

--- scripts/modules/house.js
+++ scripts/modules/house.js
@@ -2,5 +2,4 @@ import TGIFService from "./index.js"
 import { PAGES } from "./constants.js"
 
 const res = TGIFService(PAGES.HOUSE)
-console.log(res)
 
--- scripts/modules/index.js
+++ scripts/modules/index.js
@@ -1,5 +1,6 @@
 import congressService from "./services/index.service.js"
 
+
 export default async function TGIFService(param){
   let CS = await congressService(param)
   CS.results[0].members.map((data) => printTable(data)) 
@@ -7,4 +8,28 @@ export default async function TGIFService(param){
 
 function printTable(data){
   console.log(data)
+  var houseTable = `<tr>
+  <td> 
+    <a class="tH113" href="${data.url}">
+      ${data.first_name}  ${data.last_name}
+    </a>
+  </td>
+  <td> 
+  ${data.party}
+  </td>
+  <td>
+  ${data.state}
+  </td>
+  <td>
+  ${data.seniority}
+  </td>
+  <td>
+  ${data.votes_with_party_pct}%
+  </td>
+  </tr>`
+
+  var tbodyRef = document.getElementById("113-houseTable").getElementsByTagName('tbody')[0];
+  var newRow = tbodyRef.insertRow(tbodyRef.rows.length);
+ 
+  newRow.innerHTML = houseTable;
 }
