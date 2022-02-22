47aa7f75c09cbcff4f79b0ff040db2115fb13f26
Maria Jose Zelaya <majo@HP250G7NotebookPCa13ac586.fibertel.com.ar>
2022-02-21 23:04:35 -0300

    some fixes

--- scripts/modules/templates/htmlTemplates.js
+++ scripts/modules/templates/htmlTemplates.js
@@ -36,12 +36,29 @@ export const attendance = (data) => {
       </tr>`
 }
 
+
 export const attendanceGlance = (data) => {
   return `<tr>
     <td> 
         ${data.party === "R" ? 
         "Republican" : data.p === "D" ?
         "Democrats" : "Independent"}
+    </td>
+    <td> 
+    ${data.party}
+    </td>
+    <td>
+    ${data.state}
+    </td>
+    </tr>`
+  }
+
+  export const attendanceLeast = (data) {
+    return `<tr>
+    <td> 
+    <a class="tH113" href="${data.url}">
+    ${data.last_name}  ${data.first_name}
+    </a>
     </td>
     <td> 
     ${data.party}
